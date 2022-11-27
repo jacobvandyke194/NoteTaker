4e8d4d0e309c58f399fe8a16b37c425d7ded23a0
Jacob Van Dyke <jacobvandyke194@gmail.com>
2022-11-26 21:20:53 -0700

    finised get and post methods

--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const fs = require('fs');
 const PORT = 3001;
 const app = express();
+const uuid = require('./public/helpers/uuid.js');
+
+const noteData = require('./public/db/notes.json');
 //boiler plate middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //allows me to use anything in public folder without mutiple routes
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 //takes user to landing HTML page 
 app.get('/', (req, res) => {
@@ -18,8 +21,68 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
+app.get('/api/notes', (req, res) => res.json(noteData));
 
+
+app.post('/api/notes', (req, res) => {
+
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        console.log(`${req.method} request received to add a note`);
+
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        }
+
+        const response = {
+            status: 200,
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+
+        fs.readFile('./public/db/notes.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                const parsedNotes = JSON.parse(data)
+
+                parsedNotes.push(newNote);
+
+                fs.writeFile('./public/db/notes.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (err) => {
+                        console.log(err);
+                    })
+            }
+        }
+        )
+    }
+    else {
+        res.status(500).json('Error in Notes')
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    
+    fs.readFile('./public/db/notes.json', 'utf8', (err, data) => {
+        const requestedNoteId = req.params.id
+
+        if (requestedNoteId == data.noteId){
+            res.send(noteData.splice(requestedNoteId))
+        } else if(!data.id){
+            console.log('note does not exist')
+            console.log(err);
+        }
+    })
+})
 
 app.listen(PORT, () => {
-   console.log(`listening on http://localhost:${PORT}`);
+    console.log(`listening on http://localhost:${PORT}`);
 });
